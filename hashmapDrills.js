11cb781af459639b5369284134688546a4339719
Richard Tom <tomrichard14@gmail.com>
2018-09-04 09:17:28 -0700

    create hash map class

---
+++ hashmapDrills.js
@@ -0,0 +1,23 @@
+const HashMap = require('./hashmap');
+
+const main = () => {
+  const lor = new HashMap();
+  
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  
+
+  console.log(lor.get('Hobbit'));
+  // console.log(lor);
+};
+
+main();
